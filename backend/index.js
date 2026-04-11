51e59485c9fb43ea127049256910d3a9eaf6738a
AngelNifla <mnifla@unsa.edu.pe>
2026-04-11 14:28:47 -0500

    Primera version de backend con Render

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// 🔥 "Base de datos"
+const usuarios = [
+  { dni: "72029619", nombre: "Manuel Nifla Ll." },
+  { dni: "40780411", nombre: "Miguel Benites D." },
+  { dni: "40500778", nombre: "Cesar Ramirez M." },
+  { dni: "43180022", nombre: "Bernardo Cayllahue S." },
+  { dni: "48058310", nombre: "Ronald Valencia Q." },
+  { dni: "41727399", nombre: "Angel Choque Q." },
+  { dni: "72278030", nombre: "Walter Suni C." },
+  { dni: "45167507", nombre: "Freddy Cutipa C." },
+  { dni: "40950681", nombre: "Claudio Sisa C." },
+  { dni: "80385123", nombre: "Santos Lenguani Q." },
+  { dni: "24884512", nombre: "Santiago Cuyo C." },
+  { dni: "02172614", nombre: "José Quispe A." },
+  { dni: "40858173", nombre: "Sebastian Mayhua T." },
+  { dni: "71784498", nombre: "Aldair Diaz R." }
+];
+
+// 🔐 LOGIN
+app.post("/login", (req, res) => {
+  const { dni } = req.body;
+
+  const user = usuarios.find(u => u.dni === dni);
+
+  if (user) {
+    res.json({ success: true, user });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
